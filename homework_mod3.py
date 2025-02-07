7ac78b4bb52be1a9393f5cb2e1a77889b04c6017
Amvisha <amvisha@gmail.com>
2025-02-07 22:36:19 +0200

    Change parameter date

--- homework_mod3.py
+++ homework_mod3.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def get_days_from_today() -> int:
+def get_days_from_today(date) -> int:
     """
     Function returns an integer indicating the number of days from the given date to the current one
 
@@ -9,18 +9,16 @@ def get_days_from_today() -> int:
              A negative number if the specified date is in the future.
     """
     while True:
-        given_date = input("Enter the date in the format 'YYYY-MM-DD' (e.g. '2020-10-09') ")
-
         try:
-            given_date = datetime.strptime(given_date, '%Y-%m-%d').date()  # Converting a date string to a datetime object
+            date = datetime.strptime(date, '%Y-%m-%d').date()  # Converting a date string to a datetime object
             break
 
         except Exception as e: # Exception handling for incorrect date format
             print(e)
 
     today = datetime.today().date()  # Current date
-    days_from_today = (given_date - today).days  # Difference between dates
+    days_from_today = (date - today).days  # Difference between dates
     return days_from_today
 
-result: int = get_days_from_today()
+result: int = get_days_from_today('2025-02-10')
 print(f"{result} days between today's date and the given date.")
